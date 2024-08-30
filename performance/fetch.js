821da26e40d7ac03ed78bba6d54c1d02286f3ff6
MrShi <mrshi@MrShideMBP.bbrouter>
2024-08-30 11:46:21 +0800

    完善fetch请求

--- performance/fetch.js
+++ performance/fetch.js
@@ -1,25 +1,66 @@
 // fetch 请求时长
 const originalFetch = window.fetch();
 
-function overwriteFetch() {
-    window.fetch = async (...args) => {
-        const startTime = performance.now();
-        const response = await originalFetch(...args);
-        const endTime = performance.now();
-        const duration = endTime - startTime;
+// function overwriteFetch() {
+//     window.fetch = async (...args) => {
+//         const startTime = performance.now();
+//         const response = await originalFetch(...args);
+//         const endTime = performance.now();
+//         const duration = endTime - startTime;
 
+//         const reportData = {
+//             type: 'performance',
+//             subType: 'fetch',
+//             pageUrl: window.location.href,
+//             startTime,
+//             endTime,
+//             duration,
+//             status: response.status,
+//             success: response.ok,
+//         }
+
+//         // 上报数据 todo
+
+//         return response;
+//     }
+// }
+
+// 另一种写法
+function overwriteFetch() {
+    window.fetch = function newFetch(url, config) {
+        const startTime = performance.now();
         const reportData = {
             type: 'performance',
             subType: 'fetch',
             pageUrl: window.location.href,
             startTime,
-            endTime,
-            duration,
+            endTime: null,
+            duration: null,
+            status: null,
+            success: null,
+            args,
         }
+        return originalFetch(url, config)
+        .then(res => {
+            const endTime = performance.now();
+            reportData.endTime = endTime;
+            reportData.duration = endTime - startTime;
+            reportData.status = res.status;
+            reportData.success = res.ok;
+            // TODO 上报数据
 
-        // 上报数据 todo
-
-        return response;
+            return res;
+        }).catch((err) => {
+            const endTime = performance.now();
+            reportData.endTime = endTime;
+            reportData.duration = endTime - startTime;
+            reportData.status = 0;
+            reportData.success = false;
+            // TODO 上报数据
+        })
     }
 }
 
+export default fetch() {
+    overwriteFetch();
+}
