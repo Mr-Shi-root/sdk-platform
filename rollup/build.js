cdb59dff4fdd2466023098c8d262362bee38c204
MrShi <mrshi@MrShideMBP.bbrouter>
2024-09-03 18:24:48 +0800

    feat: 安装rollup进行打包

---
+++ rollup/build.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const json = require('@rollup/plugin-json')
+const babel = require('@rollup-plugin-babel')
+
+const resolveFile = function(filePath) {
+    return path.join(__dirname, filePath)
+}
+
+const plugins= [
+    json(
+        {
+            compact: true
+        }
+    ),
+    babel({
+        extensions: ['.js','.ts'],
+        babelHelpers: 'bundled',
+        presets: [
+            '@babel/env',
+            {
+                target: {
+                    browsers: [
+                        '> 1%',
+                        'last 2 versions',
+                        'not ie <= 8',
+                    ]
+                }
+            }
+        ]
+    })
+]
+
+module.exports = [
+    {
+        plugins,
+        input: resolveFile('../webEyeSDK.js'),
+        output: {
+            file: resolveFile('../dist/webEyeSDK.js'),
+            format: 'iife',
+            name: 'WebEyeSDK',
+            soucemap: true,
+            // globals: {
+            //     'vue': 'Vue',
+            // }
+        }
+    },
+    {
+        plugins,
+        input: resolveFile('../webEyeSDK.js'),
+        output: {
+            file: resolveFile('../dist/webEyeSDK.js'),
+            format: 'iife',
+            name: 'WebEyeSDK',
+            soucemap: true,
+            // globals: {
+            //     'vue': 'Vue',
+            // }
+        }
+    },
+    {
+        plugins,
+        input: resolveFile('../webEyeSDK.js'),
+        output: {
+            file: resolveFile('../dist/webEyeSDK.esm.js'),
+            format: 'esm',
+            name: 'WebEyeSDK',
+            soucemap: true,
+            // globals: {
+            //     'vue': 'Vue',
+            // }
+        }
+    },
+    {
+        plugins,
+        input: resolveFile('../webEyeSDK.js'),
+        output: {
+            file: resolveFile('../dist/webEyeSDK.cjs.js'),
+            format: 'cjs',
+            name: 'WebEyeSDK',
+            soucemap: true,//
+            // globals: {
+            //     'vue': 'Vue',
+            // }
+        }
+    }
+]
